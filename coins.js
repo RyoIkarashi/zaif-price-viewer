facd4a00654b515e887a50cda2ce4f828d135333
Ryo Ikarashi <ryo.5630@gmail.com>
2017-11-02 23:51:04 +0900

    initial commit

---
+++ coins.js
@@ -0,0 +1,86 @@
+module.exports = {
+  bitcoin: {
+    name: 'BITCOIN',
+    unit: 'btc',
+    image: '',
+    isTradePath: false
+  },
+  ethereum: {
+    name: 'ETHEREUM',
+    unit: 'eth',
+    image: '',
+    isTradePath: true
+  },
+  nem: {
+    name: 'NEM',
+    unit: 'xem',
+    image: '',
+    isTradePath: false
+  },
+  bitcoin_cash: {
+    name: 'BITCOIN CASH',
+    unit: 'bch',
+    image: '',
+    isTradePath: true
+  },
+  mona: {
+    name: 'MONA',
+    unit: 'mona',
+    image: '',
+    isTradePath: false
+  },
+  sjcx: {
+    name: 'SJCX',
+    unit: 'sjcx',
+    image: '',
+    isTradePath: true
+  },
+  zen: {
+    name: 'ZEN',
+    unit: 'jpyz',
+    image: '',
+    isTradePath: true
+  },
+  ncxc: {
+    name: 'NCXC',
+    unit: 'ncxc',
+    image: '',
+    isTradePath: true
+  },
+  cicc: {
+    name: 'CICC',
+    unit: 'cicc',
+    image: '',
+    isTradePath: true
+  },
+  pepecash: {
+    name: 'PEPECACHE',
+    unit: 'pepecash',
+    image: '',
+    isTradePath: true
+  },
+  xcp: {
+    name: 'XCP',
+    unit: 'xcp',
+    image: '',
+    isTradePath: true
+  },
+  bitcrystals: {
+    name: 'BITCRYSTALS',
+    unit: 'bitcrystals',
+    image: '',
+    isTradePath: true
+  },
+  zaif: {
+    name: 'ZAIF',
+    unit: 'zaif',
+    image: '',
+    isTradePath: true
+  },
+  fscc: {
+    name: 'FSCC',
+    unit: 'fscc',
+    image: '',
+    isTradePath: true
+  },
+};
