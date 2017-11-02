7050be9e72e2399e2afcf5f4d2f8395b7c742134
Ryo Ikarashi <ryo.5630@gmail.com>
2017-11-03 00:32:17 +0900

    add metadata

--- zaif.10s.js
+++ zaif.10s.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env /usr/local/bin/node
 
+# <bitbar.title>Zaif Price Viewer</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Ryo Ikarashi</bitbar.author>
+# <bitbar.author.github>RyoIkarashi</bitbar.author.github>
+# <bitbar.desc>Display the spot JPY prices of cryptocurrencies and your current balance in zaif.jp</bitbar.desc>
+# <bitbar.image>https://user-images.githubusercontent.com/5750408/32333718-ec1d99e6-c02b-11e7-8990-c26f6629a1df.png</bitbar.image>
+# <bitbar.dependencies>node</bitbar.dependencies>
+# <bitbar.abouturl>https://github.com/RyoIkarashi/zaif-price-viewer</bitbar.abouturl>
+
+# If you feel this little tool gives you some value, tips are always welcome at the following addresses!
+# Bitcoin: 1DrLPjzmNHtkdBstd82xvCxGY38PnKauRH
+# Mona:    MC7XMmi1YXoJH19D1q4H8ijBkdvarWBTMi
+
 const bitbar = require('bitbar');
 const zaif = require('zaif.jp');
 const axios = require('axios');
