b226f2dc4cc8fbe103b54d2505185535094632b9
Ryo Ikarashi <ryo.5630@gmail.com>
2017-12-04 19:01:18 +0900

    add comsa

--- coins.js
+++ coins.js
@@ -4,97 +4,111 @@ module.exports = {
     unit: 'btc',
     image: '',
     isTradePath: false,
-    type: 'currency',
+    type: 'currency'
   },
   ethereum: {
     name: 'ETHEREUM',
     unit: 'eth',
     image: '',
     isTradePath: true,
-    type: 'currency',
+    type: 'currency'
+  },
+  mosaic_comsa: {
+    name: 'ERC20:COMSA',
+    unit: 'erc20.cms',
+    image: '',
+    isTradePath: true,
+    type: 'comsa'
+  },
+  erc20_comsa: {
+    name: 'MOSAIC:COMSA',
+    unit: 'mosaic.cms',
+    image: '',
+    isTradePath: true,
+    type: 'comsa'
   },
   nem: {
     name: 'NEM',
     unit: 'xem',
     image: '',
     isTradePath: false,
-    type: 'currency',
+    type: 'currency'
   },
   bitcoin_cash: {
     name: 'BITCOIN CASH',
     unit: 'bch',
     image: '',
     isTradePath: true,
-    type: 'currency',
+    type: 'currency'
   },
   mona: {
     name: 'MONA',
     unit: 'mona',
     image: '',
     isTradePath: false,
-    type: 'currency',
+    type: 'currency'
   },
   sjcx: {
     name: 'SJCX',
     unit: 'sjcx',
     image: '',
     isTradePath: true,
-    type: 'token',
+    type: 'token'
   },
   zen: {
     name: 'ZEN',
     unit: 'jpyz',
     image: '',
     isTradePath: true,
-    type: 'token',
+    type: 'token'
   },
   ncxc: {
     name: 'NCXC',
     unit: 'ncxc',
     image: '',
     isTradePath: true,
-    type: 'token',
+    type: 'token'
   },
   cicc: {
     name: 'CICC',
     unit: 'cicc',
     image: '',
     isTradePath: true,
-    type: 'token',
+    type: 'token'
   },
   pepecash: {
     name: 'PEPECACH',
     unit: 'pepecash',
     image: '',
     isTradePath: true,
-    type: 'token',
+    type: 'token'
   },
   xcp: {
     name: 'XCP',
     unit: 'xcp',
     image: '',
     isTradePath: true,
-    type: 'token',
+    type: 'token'
   },
   bitcrystals: {
     name: 'BITCRYSTALS',
     unit: 'bitcrystals',
     image: '',
     isTradePath: true,
-    type: 'token',
+    type: 'token'
   },
   zaif: {
     name: 'ZAIF',
     unit: 'zaif',
     image: '',
     isTradePath: true,
-    type: 'token',
+    type: 'token'
   },
   fscc: {
     name: 'FSCC',
     unit: 'fscc',
     image: '',
     isTradePath: true,
-    type: 'token',
-  },
+    type: 'token'
+  }
 };
--- zaif.10s.js
+++ zaif.10s.js
@@ -14,85 +14,122 @@
 // Mona:    MC7XMmi1YXoJH19D1q4H8ijBkdvarWBTMi
 
 const bitbar = require('bitbar');
-const zaif   = require('zaif.jp');
-const axios  = require('axios');
-let COINS    = require('./coins');
-const ENV    = require('./env.json');
+const zaif = require('zaif.jp');
+const axios = require('axios');
+let COINS = require('./coins');
+const ENV = require('./env.json');
 const COMPARATIVE_UNIT = 'jpy';
 
 COINS = Object.keys(COINS).map(coin => {
-  COINS[coin].unit = COINS[coin].isTradePath ? COINS[coin].unit.toUpperCase() : COINS[coin].unit
-  return COINS[coin]
-})
+  COINS[coin].unit = COINS[coin].isTradePath
+    ? COINS[coin].unit.toUpperCase()
+    : COINS[coin].unit;
+  return COINS[coin];
+});
 
-const privateApi = zaif.createPrivateApi(ENV.access_key, ENV.secret_key, 'user agent is node-zaif');
-const publicApi  = zaif.PublicApi;
+const privateApi = zaif.createPrivateApi(
+  ENV.access_key,
+  ENV.secret_key,
+  'user agent is node-zaif'
+);
+const publicApi = zaif.PublicApi;
 const getBalance = privateApi.getInfo();
 
-const getAllCoinsTrades = Object.keys(COINS).map(coin => publicApi.trades(`${COINS[coin].unit}_${COMPARATIVE_UNIT}`));
-const getAllCoinsRate   = Object.keys(COINS).map(coin => publicApi.lastPrice(`${COINS[coin].unit}_${COMPARATIVE_UNIT}`));
-const mergeCoinsAndInfo = (info) => Object.keys(COINS).map((coin, index) => {
-  const { rates, balances, trades } = info;
-  return Object.assign({}, COINS[coin], {
-    rate: rates[index].hasOwnProperty('last_price') ? rates[index].last_price : 0,
-    trades: trades[index],
-    balance: balances.hasOwnProperty(COINS[coin].unit) ? balances[COINS[coin].unit] : 0,
-  })
-});
-
-const floatFormat = (number, n) => {
-  const _pow = Math.pow(10 , n) ;
-  return Math.round(number * _pow) / _pow;
-}
-
-axios.all([...getAllCoinsRate, ...getAllCoinsTrades, getBalance]).then(result => {
-  const rates    = result.slice(0, getAllCoinsRate.length);
-  const trades   = result.slice(getAllCoinsRate.length, getAllCoinsRate.length + getAllCoinsTrades.length);
-  const balances = result.slice(getAllCoinsRate.length + getAllCoinsTrades.length)[0].funds;
-
-  let totalBalance = 0;
-
-  const coinsInfo  = mergeCoinsAndInfo({ rates, balances, trades });
-  const currencies = coinsInfo.filter(coin => coin.type === 'currency');
-  const tokens     = coinsInfo.filter(coin => coin.type === 'token');
-
-  const bitbarContent = (coins) => coins.map((coin, index) => {
-    const { name, image, unit, rate, balance, isTradePath, trades } = coin;
-
-    const bids = trades.filter(trade => trade.trade_type === 'bid').map(bid => bid.amount);
-    const asks = trades.filter(trade => trade.trade_type === 'ask').map(ask => ask.amount);
-
-    const totalBids = bids.reduce((sum, value) => sum + value * rate, 1);
-    const totalAsks = asks.reduce((sum, value) => sum + value * rate, 1);
-
-    const difference = totalBids - totalAsks;
-    const prefix     = difference > 0 ? '+' : '';
-
-    totalBalance += Number(rate) * Number(balance);
-
-    return {
-      text: `[${unit.toUpperCase()}] ¥${rate} (¥${prefix}${floatFormat(difference, 3)}) / ¥${floatFormat(Number(rate) * Number(balance), 3)}`,
-      color: difference < 0 ? ENV.colors.red : ENV.colors.green,
-      href: `https://zaif.jp/trade${isTradePath ? '/' : '_'}${unit.toLowerCase()}_jpy`
-    };
+const getAllCoinsTrades = Object.keys(COINS).map(coin =>
+  publicApi.trades(`${COINS[coin].unit}_${COMPARATIVE_UNIT}`)
+);
+const getAllCoinsRate = Object.keys(COINS).map(coin =>
+  publicApi.lastPrice(`${COINS[coin].unit}_${COMPARATIVE_UNIT}`)
+);
+const mergeCoinsAndInfo = info =>
+  Object.keys(COINS).map((coin, index) => {
+    const { rates, balances, trades } = info;
+    return Object.assign({}, COINS[coin], {
+      rate: rates[index].hasOwnProperty('last_price')
+        ? rates[index].last_price
+        : 0,
+      trades: trades[index],
+      balance: balances.hasOwnProperty(COINS[coin].unit)
+        ? balances[COINS[coin].unit]
+        : 0
+    });
   });
 
-  const currenciesContent = bitbarContent(currencies);
-  const tokensContent = bitbarContent(tokens);
+const floatFormat = (number, n) => {
+  const _pow = Math.pow(10, n);
+  return Math.round(number * _pow) / _pow;
+};
 
-  bitbar([
-    `¥${floatFormat(totalBalance, 3)}`,
-    bitbar.sep,
-    "CURRENCIES",
-    ...currenciesContent,
-    bitbar.sep,
-    "TOKENS",
-    ...tokensContent,
-    bitbar.sep,
-    "JPY",
-    `¥${balances.jpy}`,
-    bitbar.sep,
-    "Total Balance (JPY inc)",
-    `¥${floatFormat(totalBalance + balances.jpy, 3)}`,
-  ]);
-});
+axios
+  .all([...getAllCoinsRate, ...getAllCoinsTrades, getBalance])
+  .then(result => {
+    const rates = result.slice(0, getAllCoinsRate.length);
+    const trades = result.slice(
+      getAllCoinsRate.length,
+      getAllCoinsRate.length + getAllCoinsTrades.length
+    );
+    const balances = result.slice(
+      getAllCoinsRate.length + getAllCoinsTrades.length
+    )[0].funds;
+
+    let totalBalance = 0;
+
+    const coinsInfo = mergeCoinsAndInfo({ rates, balances, trades });
+    const currencies = coinsInfo.filter(coin => coin.type === 'currency');
+    const tokens = coinsInfo.filter(coin => coin.type === 'token');
+    const comsa = coinsInfo.filter(coin => coin.type === 'comsa');
+
+    const bitbarContent = coins =>
+      coins.map((coin, index) => {
+        const { name, image, unit, rate, balance, isTradePath, trades } = coin;
+
+        const bids = trades
+          .filter(trade => trade.trade_type === 'bid')
+          .map(bid => bid.amount);
+        const asks = trades
+          .filter(trade => trade.trade_type === 'ask')
+          .map(ask => ask.amount);
+
+        const totalBids = bids.reduce((sum, value) => sum + value * rate, 1);
+        const totalAsks = asks.reduce((sum, value) => sum + value * rate, 1);
+
+        const difference = totalBids - totalAsks;
+        const prefix = difference > 0 ? '+' : '';
+
+        totalBalance += Number(rate) * Number(balance);
+
+        return {
+          text: `[${unit.toUpperCase()}] ¥${rate} (¥${prefix}${floatFormat(
+            difference,
+            3
+          )}) / ¥${floatFormat(Number(rate) * Number(balance), 3)}`,
+          color: difference < 0 ? ENV.colors.red : ENV.colors.green,
+          href: `https://zaif.jp/trade${
+            isTradePath ? '/' : '_'
+          }${unit.toLowerCase()}_jpy`
+        };
+      });
+
+    const currenciesContent = bitbarContent(currencies);
+    const tokensContent = bitbarContent(tokens);
+    const comsaContent = bitbarContent(comsa);
+
+    bitbar([
+      `¥${floatFormat(totalBalance, 3)}`,
+      bitbar.sep,
+      'CURRENCIES',
+      ...currenciesContent,
+      bitbar.sep,
+      'COMSA',
+      ...comsaContent,
+      bitbar.sep,
+      'TOKENS',
+      ...tokensContent,
+      bitbar.sep,
+      'JPY',
+      `¥${balances.jpy}`,
+      bitbar.sep,
+      'Total Balance (JPY inc)',
+      `¥${floatFormat(totalBalance + balances.jpy, 3)}`
+    ]);
+  });
